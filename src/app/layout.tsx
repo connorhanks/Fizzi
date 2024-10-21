f85487c514e463e65be20f264e15a79eb16484ec
connorhanks <hello@connorhanks.com>
2024-10-21 12:36:45 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+import Header from "@/components/Header";
+
+// Needs to be public for Three.js
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden">
+        <Header />
+        <main>{children}</main>
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
